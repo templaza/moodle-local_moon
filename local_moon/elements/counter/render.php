a5f349cf63ae15de6f00c8eeb109ce67ca104437
Sonny Le <sonlv@templaza.com>
2025-12-09 14:37:06 +0700

    Add Counter Widget

--- local_moon/elements/counter/render.php
+++ local_moon/elements/counter/render.php
@@ -1,87 +1,106 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
+use local_moon\library\Framework;
 use local_moon\library\Helper\Style;
 use local_moon\library\Helper\SubForm;
 $params = $this->params;
 $element = $this;
-$list_items     = new SubForm($params->get('list_items', ''));
-if (!count($list_items->getData())) {
+$items     = new SubForm($params->get('items', ''));
+if (!count($items->data)) {
     return false;
 }
-$title_html_element =   $params->get('title_html_element', 'h3');
-$title_font_style   =   $params->get('title_font_style');
-if (!empty($title_font_style)) {
-    Style::renderTypography('#'.$element->id.' .as-list-title', $title_font_style, null, $element->isRoot);
+$document = Framework::getDocument();
+$style = $element->style;
+$style_dark = $element->style_dark;
+$row_column_cls     =   '';
+
+$responsive_key     =   ['xxl', 'xl', 'lg', 'md', 'sm', 'xs'];
+foreach ($responsive_key as $key) {
+    $default        =   match ($key) {
+        'xxl', 'xl' =>  '',
+        'lg'        =>  3,
+        default     =>  1
+    };
+    $column         =   $params->get($key . '_column', $default);
+
+    if ($key !== 'xs') {
+        $row_column_cls     .=  $column ? ' row-cols-'. $key .'-' . $column : '';
+        $row_gutter         =   $params->get('row_gutter_'.$key, '');
+        $column_gutter      =   $params->get('column_gutter_'. $key, '');
+        if ($row_gutter) {
+            $row_column_cls .=  ' gy-' . $key . '-' . $row_gutter;
+        }
+        if ($column_gutter) {
+            $row_column_cls .=  ' gx-' . $key . '-' . $column_gutter;
+        }
+    } else {
+        $row_column_cls     .=  $column ? ' row-cols-' . $column : '';
+        $row_gutter         =   $params->get('row_gutter', 3);
+        $column_gutter      =   $params->get('column_gutter', 3);
+        $row_column_cls     .=  ' gy-' . $row_gutter;
+        $row_column_cls     .=  ' gx-' . $column_gutter;
+    }
 }
-$title_heading_margin=  $params->get('title_heading_margin', '');
+
+$card_style         =   $params->get('card_style', '');
+$card_style         =   $card_style ? ' text-bg-' . $card_style : '';
+
+$card_size          =   $params->get('card_size', '');
+$card_size          =   $card_size ? ' card-size-' . $card_size : '';
+
+$card_rounded_size  =   $params->get('card_rounded_size', '3');
+$border_radius      =   $params->get('card_border_radius', '');
+$bd_radius          =   $border_radius != '' ? ' rounded-' . $border_radius : ' rounded-' . $card_rounded_size;
+
+$enable_grid_match  =   $params->get('enable_grid_match', 0);
+
+$box_shadow         =   $params->get('card_box_shadow', '');
+$box_shadow         =   $box_shadow ? ' ' . $box_shadow : '';
+$box_shadow_hover   =   $params->get('card_box_shadow_hover', '');
+$box_shadow_hover   =   $box_shadow_hover ? ' ' . $box_shadow_hover : '';
 
 $content_font_style =   $params->get('content_font_style');
 if (!empty($content_font_style)) {
-    Style::renderTypography('#'.$element->id.' .as-list-desc', $content_font_style, null, $element->isRoot);
+    Style::renderTypography('#'.$element->id.' .moon-text', $content_font_style, null, $element->isRoot);
 }
 
-$item_margin    =   $params->get('item_margin', '');
-$item_padding   =   $params->get('item_padding', '');
-$list_style     =   $params->get('list_style', 'ul');
-$title_width    =   intval($params->get('title_width', 3));
+$transition     = $params->get('hover_transition', '');
+$transition     = $transition !== '' ? ' as-transition-' . $transition : '';
 
-$tag = match ($list_style) {
-    'ol', 'list-group-numbered' => 'ol',
-    'list-description' => 'dl',
-    default => 'ul',
-};
+$card_hover_transition     = $params->get('card_hover_transition', '');
+$card_hover_transition     = $card_hover_transition !== '' ? ' as-transition-' . $card_hover_transition : '';
 
-$class = match ($list_style) {
-    'list-unstyled', 'list-inline', 'list-group' => $list_style,
-    'list-group-flush', 'list-group-numbered' => 'list-group '. $list_style,
-    'list-description' => 'row',
-    default => ''
-};
+$use_masonry        =   $params->get('use_masonry', 0);
+echo '<div class="row'.($use_masonry ? ' as-masonry as-loading' : '').$row_column_cls.'">';
+foreach ($items->data as $key => $grid) {
 
-$class_item = match ($list_style) {
-    'list-group', 'list-group-flush', 'list-group-numbered' => 'list-item list-group-item d-flex align-items-start',
-    'list-inline' => 'list-item list-inline-item',
-    default => 'list-item'
-};
+    echo '<div id="grid-'. $grid -> id .'" class="as-grid"><div class="card' . $card_style . $box_shadow . $box_shadow_hover .$bd_radius . $card_hover_transition . ($enable_grid_match ? ' h-100' : '') . '">';
 
-$class_item_inner = match ($list_style) {
-    'list-group-numbered' => ' ms-2',
-    default => ''
-};
+    echo '<div class="card-body' . $card_size . '">'; // Start Card-Body
 
-echo '<'.$tag.' class="' . $class . '">';
-foreach ($list_items->getData() as $list) {
-    $icon_type      =   $list->params->get('icon_type', 'fontawesome');
-    if ($icon_type === 'fontawesome') {
-        $icon       =   $list->params->get('fa_icon', '');
-    } else {
-        $icon       =   $list->params->get('custom_icon', '');
+    if (!empty($grid->params->get('title', ''))) {
+        echo '<div class="moon-number">' . $grid->params->get('title', '') . '</div>';
     }
-    $title          =   ($icon ? '<i class="'.$icon.' me-2"></i>' : '').$list->params->get('title', '');
-    $description    =   $list->params->get('description', '');
-    if ($list_style === 'list-description') {
-        echo '<dt class="as-list-title col-'.$title_width.'">'.$title.'</dt>';
-        echo '<dd class="as-list-desc col-'.($title_width < 12 ? 12-$title_width : 12).'">'.$description.'</dd>';
-    } else {
-        echo '<li class="'.$class_item.'">';
-        echo '<div class="list-item-inner'.$class_item_inner.'">';
-        echo $title ? '<'.$title_html_element.' class="as-list-title">'. $title . '</'.$title_html_element.'>' : '';
-        echo $description ? '<div class="as-list-desc">'. $description . '</div>' : '';
-        echo '</div>';
-        echo '</li>';
+
+    echo '</div>'; // End Card-Body
+
+    echo '</div></div>';
+}
+echo '</div>';
+if ($use_masonry) {
+    $document->loadMasonry('#'. $element->id .' .as-masonry');
+}
+if ($params->get('card_style', '') == 'custom') {
+    $text_color     =   Style::getColor($params->get('text_color', ''));
+    $style->child('.as-grid > .card')->addCss('color', $text_color['light']);
+    $style_dark->child('.as-grid > .card')->addCss('color', $text_color['dark']);
+
+    $bg_color       =   Style::getColor($params->get('bg_color', ''));
+    $style->child('.as-grid > .card')->addCss('background-color', $bg_color['light']);
+    $style_dark->child('.as-grid > .card')->addCss('background-color', $bg_color['dark']);
+
+    $card_border    =   json_decode($params->get('card_border', ''), true);
+    if (!empty($card_border)) {
+        Style::addBorderStyle('#'. $element->id . ' .as-grid > .card', $card_border, 'global', $element->isRoot);
     }
-}
-echo '</'.$tag.'>';
-
-if (!empty($title_heading_margin)) {
-    Style::setSpacingStyle($element->style->child('.as-list-title'), $title_heading_margin, 'margin');
-}
-
-// Item Margin
-if (!empty($item_margin)) {
-    Style::setSpacingStyle($element->style->child('.list-item'), $item_margin, 'margin');
-}
-// Item Padding
-if (!empty($item_padding)) {
-    Style::setSpacingStyle($element->style->child('.list-item'), $item_padding);
 }
