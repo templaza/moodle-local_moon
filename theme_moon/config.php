e01cb18ffbd98920917d90b6014deb42db614a74
Sonny Le <sonlv@templaza.com>
2026-08-13 16:05:55 +0700

    Init theme Moon

--- theme_moon/config.php
+++ theme_moon/config.php
@@ -10,7 +10,6 @@ $THEME->enable_dock = false;
 $THEME->yuicssmodules = array();
 $THEME->requiredblocks = '';
 $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
-$THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->scss = function($theme) {
     return theme_moon_get_main_scss_content($theme);
 };
