36a217d7fb0fee9bf6820e4fd17692ae6e1cd787
Sonny Le <sonlv@templaza.com>
2026-08-28 08:40:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,6 @@ $document->add_script_options('astroid_lang', settings::load_language());
 echo $OUTPUT->render_from_template('local_moon/manage', [
     'title' => get_string('pluginname', $theme->get_name()) . get_string('pluginname_subfix', 'local_moon'),
     'favicon' => $OUTPUT->image_url('favicon', 'theme'),
-    'content' => 'This demo page proves the framework assets, template and classes load correctly.',
     'color_mode_theme' => 'light',
     'script_options' => json_encode($document->get_script_options()),
     'stylesheets' => '<link href="' . parse_url($CFG->wwwroot, PHP_URL_PATH) . '/local/moon/assets/manage/index.css' . '" rel="stylesheet" type="text/css" /><link href="' . parse_url($CFG->wwwroot, PHP_URL_PATH) . '/local/moon/assets/fontawesome/css/all.min.css' . '" rel="stylesheet" type="text/css" /><link href="' . parse_url($CFG->wwwroot, PHP_URL_PATH) . '/local/moon/assets/linearicons/font.min.css' . '" rel="stylesheet" type="text/css" />',
