e6c35fcb6fa77d8e10eb483eb07f3c55c6c0ad27
Sonny Le <sonlv@templaza.com>
2026-09-18 09:10:21 +0700

    Correct version information

--- version.php
+++ version.php
@@ -24,7 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_moon';
-$plugin->version   = 2026091001;
-$plugin->requires  = 2024010100; // Targeting Moodle 5.0+ (adjust if needed)
+$plugin->version   = 2026091800;
+$plugin->requires  = 2023100900;
+$plugin->supported = [403, 522];
 $plugin->release   = '1.0.9';
 $plugin->maturity  = MATURITY_STABLE;
